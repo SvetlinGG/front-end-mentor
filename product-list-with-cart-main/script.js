3acc10386caf6fdb50252d1a1b72dc9d754c34ee
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-23 15:12:19 +0200

    take a price of the product

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,5 +1,20 @@
+function addProduct() {
+    let totalPrice = 0; 
+  
+    const button = document.getElementById('add-waffel');
+    const priceEl = document.getElementById('price-waffel');
+  
+    button.addEventListener('click', () => {
+      
+      const price = parseFloat(priceEl.textContent.replace('$', '').trim());
+  
+      totalPrice += price;
+  
+      console.log("Added product price:", price);
+      console.log("Total price so far:", totalPrice);
+    });
+  }
+  
+  addProduct();
+  
 
-    const product = {
-        waffel: document.getElementById('add-waffel'),
-        
-    }
