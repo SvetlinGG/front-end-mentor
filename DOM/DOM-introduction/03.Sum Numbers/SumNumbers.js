0de8a627ec976da9e83b0640853a72520d56c0a0
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-22 00:24:34 +0200

    complete

--- DOM/DOM-introduction/03.Sum Numbers/SumNumbers.js
+++ DOM/DOM-introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-
+    let a = document.getElementById('num1');
+    let b = document.getElementById('num2');
+    let sum = document.getElementById('sum');
+    sum.value = Number(a.value) + Number(b.value);
 }
