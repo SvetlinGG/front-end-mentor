23c9c9d52eb78a75d181eeb6ae901c92104d2c75
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-22 15:45:26 +0200

    sum table

--- DOM/DOM-introduction/05.Colorize Table/ColorizeTable.js
+++ DOM/DOM-introduction/05.Colorize Table/ColorizeTable.js
@@ -1,9 +1,3 @@
 function colorize() {
 
-   const selectRows = document.querySelectorAll('table tr:nth-child(even)');
-   for ( let row of selectRows){
-    row.style.background = 'lightblue'
-   }
-   
-  
-}
+   con
