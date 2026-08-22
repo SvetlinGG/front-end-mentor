86438e0f95e1309cc6247c81f1cc80ba389235b5
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-22 15:35:52 +0200

    complete task

--- DOM/DOM-introduction/05.Colorize Table/ColorizeTable.js
+++ DOM/DOM-introduction/05.Colorize Table/ColorizeTable.js
@@ -1,4 +1,9 @@
 function colorize() {
 
+   const selectRows = document.querySelectorAll('table tr:nth-child(even)');
+   for ( let row of selectRows){
+    row.style.background = 'lightblue'
+   }
    
+  
 }
