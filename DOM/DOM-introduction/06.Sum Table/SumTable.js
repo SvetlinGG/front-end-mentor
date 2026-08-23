c539c97ccbc24b5e79912733341e7f71fdc90454
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-23 22:12:01 +0200

    fix the issue

--- DOM/DOM-introduction/06.Sum Table/SumTable.js
+++ DOM/DOM-introduction/06.Sum Table/SumTable.js
@@ -1,11 +1,18 @@
 function sumTable() {
     let total = 0;
-    let productPrice = document.querySelectorAll('table td');
+    let productPrice = document.querySelectorAll('tr td:nth-child(2)');
     let sum = document.getElementById('sum');
     
+    
+    
     for (let price of productPrice){
-        total += price;
-        sum.value = total;
+        total += Number(price.textContent)
+
+        
+        
     }
+    sum.value = total
+    console.log(sum.value);
+    
 
 }
