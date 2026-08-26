196735311cfd7a2300283c99de8103e053df1fbc
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-26 21:36:12 +0200

    complete task

--- DOM/DOM-itroduction-EXE/04. Search in List/search.js
+++ DOM/DOM-itroduction-EXE/04. Search in List/search.js
@@ -1,4 +1,14 @@
 function search() {
    
-   
-}
+   const towns = document.querySelectorAll('#towns li');
+   const inputText = document.querySelector('#searchText').value;
+   const matches = document.querySelector('#result');
+   let count = 0;
+
+   if ( inputText === '' || inputText === ' '){
+      matches.textContent = '0 matches found'
+   }else{
+      towns.forEach(town => {
+         if (town.textContent.includes(inputText)){
+            count ++
+            matches.textContent = `${count}
