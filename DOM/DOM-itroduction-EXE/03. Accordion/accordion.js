9e504653c97446e55136a60f4bc9c006eb7472ec
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-26 20:35:30 +0200

    check accordion

--- DOM/DOM-itroduction-EXE/03. Accordion/accordion.js
+++ DOM/DOM-itroduction-EXE/03. Accordion/accordion.js
@@ -1,5 +1,13 @@
 function toggle() {
     
-    
+    let accordion = document.getElementById('accordion');
+    let text = document.getElementById('extra');
+    let button = document.getElementsByClassName('button');
+
+    if (button.value === 'More'){
+        text.style.direction = 'block'
+    }else if (text.style.display === 'block'){
+        text.style.display = 'none'
+    }
     
 }
