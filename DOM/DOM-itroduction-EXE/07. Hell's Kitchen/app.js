d0b0b3fd7a1217d15d4ba3617ff741ec9cc7332a
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-30 20:30:53 +0200

    set movies

--- DOM/DOM-itroduction-EXE/07. Hell's Kitchen/app.js
+++ DOM/DOM-itroduction-EXE/07. Hell's Kitchen/app.js
@@ -156,4 +156,56 @@ function solve() {
 		const workersEl = document.getElementsByTagName('p')[1];
 		workersEl.textContent = '';
 		bestRestaurant.textContent = '';
-		// creat
+		// create collection
+		const restaurants = {};
+
+		// parse the input
+		for (const line of input) {
+			let [restaurantName, workers] = line.split(' - ');
+			workers = workers.split(', ');
+
+			if (!restaurants.hasOwnProperty(restaurantName)) {
+				restaurants[restaurantName] = {
+					workers: workers,
+					bestSalary: 0,
+					averageSalary: 0,
+				};
+			} else {
+				workers.forEach(worker =>
+					restaurants[restaurantName].workers.push(worker)
+				);
+			}
+
+			// calculate the best salary
+			restaurants[restaurantName].bestSalary = getTheHighestSalary(
+				restaurants[restaurantName]
+			);
+
+			// calculate the average salary // income
+			restaurants[restaurantName].averageSalary = calcAverageSalary(
+				restaurants[restaurantName]
+			);
+		}
+
+		// get the best restaurant
+		const best = Object.entries(restaurants).sort(
+			(a, b) => b[1].averageSalary - a[1].averageSalary
+		)[0];
+		const [name, obj] = best;
+		const { bestSalary, averageSalary, workers } = obj;
+
+		// render the best restaurant
+		bestRestaurant.textContent = `Name: ${name} Average Salary: ${averageSalary.toFixed(
+			2
+		)} Best Salary: ${bestSalary.toFixed(2)}`;
+
+		// render the workers
+		const sortedBySalaries = workers
+			.map(el => el.split(' '))
+			.sort((a, b) => b[1] - a[1]);
+
+		sortedBySalaries.forEach(el => {
+			const [name, salary] = el;
+			workersEl.textContent += `Name: ${name} With Salary: ${salary} `;
+		});
+	}*/
--- DOM/DOM-manipulation-LAB/01. List-Of-Items/movies/movie.js
+++ DOM/DOM-manipulation-LAB/01. List-Of-Items/movies/movie.js
@@ -1,25 +1,14 @@
 
-const movieUlElement = document.getElementById('movie-list');
-const firstMovieList = document.createElement('li');
-const secondMovieList = document.createElement('li');
-const thirdMovieList = document.createElement('li');
-firstMovieList.textContent = 'Star Wars';
-secondMovieList.textContent = 'Man of Steel'
-thirdMovieList.textContent = 'HEAT'
+const moviesList = document.getElementById('movie-list');
+const firstMovie = document.createElement('li');
+const secondMovie = document.createElement('li');
+const thirdMovie = document.createElement('li');
+
+firstMovie.textContent = 'Star Wars';
+secondMovie.textContent = 'Lord of The Rings';
+thirdMovie.textContent = 'Harry Potter';
 
 
-
-setTimeout( () => { 
-     movieUlElement.appendChild(firstMovieList);
-    }, 4000);
-movieUlElement.appendChild(secondMovieList)
-movieUlElement.appendChild(thirdMovieList);
-
-
-const newEl = document.createElement('a');
-newEl.href = 'https://www.imdb.com/title/tt0133093/?ref_=fn_al_tt_1';
-newEl.textContent = 'The Matrix';
-newEl.target = '_blank';
-
-const theMatrix = document.createElement('li');
-theMatrix.appendChild(newEl)
+moviesList.append(firstMovie);
+moviesList.append(secondMovie);
+moviesList.append(thirdMovie);
