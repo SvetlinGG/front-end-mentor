50a469454643f4ec53008912d1e803a9ea43a5a8
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-30 15:51:25 +0200

    take selectors

--- DOM/DOM-itroduction-EXE/07. Hell's Kitchen/app.js
+++ DOM/DOM-itroduction-EXE/07. Hell's Kitchen/app.js
@@ -1,63 +1,81 @@
 function solve() {
 
 	document.querySelector('#btnSend').addEventListener('click', onClick);
-	const input = document.querySelector('#inputs>textarea');
-	const bestRestorantResult = document.querySelector('#bestRestorant');
-	const workersResult = document.querySelector('#workers>p');
+
+	const textAreaRef = document.querySelector('textarea');
+	const bestRestaurantRef = document.querySelector('#bestRestaurant p');
+	const workersRef = document.querySelector('#workers p');
 
 
-	function onClick(){
-		const arr = JSON.parse(input.value );
 
-		const restorants = {};
 
-		arr.forEach(el => {
-			const[ name, workers] = el.split(' - ');
-			const workersCollection = workers.split(', ');
-			const workersDictCollection = [];
-			for ( let worker of workersCollection ){
-				const [workerName, salary]= worker.split(' ');
+
+
+
+
+
+
+
+
+
+	// document.querySelector('#btnSend').addEventListener('click', onClick);
+	// const input = document.querySelector('#inputs>textarea');
+	// const bestRestorantResult = document.querySelector('#bestRestorant');
+	// const workersResult = document.querySelector('#workers>p');
+
+
+	// function onClick(){
+	// 	const arr = JSON.parse(input.value );
+
+	// 	const restorants = {};
+
+	// 	arr.forEach(el => {
+	// 		const[ name, workers] = el.split(' - ');
+	// 		const workersCollection = workers.split(', ');
+	// 		const workersDictCollection = [];
+	// 		for ( let worker of workersCollection ){
+	// 			const [workerName, salary]= worker.split(' ');
 				
-				workersDictCollection.push({ name: workerName, salary});
-			}
-			if ( restorants[name]){
-				workersDictCollection = workersDictCollection.concat(restorants[name].workers);
-			}
-			workersDictCollection.sort( (w1, w2) => w2.salary - w1.salary)
-			const bestSalary = workersDictCollection[0].salary;
-			const totalSalary = workersDictCollection.reduce((sum, w) => sum + w.salary, 0);
-			const avgSalary = totalSalary / workersDictCollection.length;
-			restorants[name] = {
-				workers: workersDictCollection,
-				avgSalary: avgSalary,
-				bestSalary,
-			};
+	// 			workersDictCollection.push({ name: workerName, salary});
+	// 		}
+	// 		if ( restorants[name]){
+	// 			workersDictCollection = workersDictCollection.concat(restorants[name].workers);
+	// 		}
+	// 		workersDictCollection.sort( (w1, w2) => w2.salary - w1.salary)
+	// 		const bestSalary = workersDictCollection[0].salary;
+	// 		const totalSalary = workersDictCollection.reduce((sum, w) => sum + w.salary, 0);
+	// 		const avgSalary = totalSalary / workersDictCollection.length;
+	// 		restorants[name] = {
+	// 			workers: workersDictCollection,
+	// 			avgSalary: avgSalary,
+	// 			bestSalary,
+	// 		};
 
-		});
-		let bestRestaurantResultSalary = 0;
-		let bestRestaurant = undefined;
+	// 	});
+	// 	let bestRestaurantResultSalary = 0;
+	// 	let bestRestaurant = undefined;
 
-		for ( let name in restorants){
-			const  currentRestorant = restorants[name];
-			if( currentRestorant.avgSalary > bestRestaurantResultSalary){
-				bestRestaurant = {
-					name,
-					workers: currentRestorant[name].workers,
-					bestSalary: currentRestorant[name].bestSalary,
-					avgSalary: currentRestorant[name].avgSalary
-				};
-				bestRestaurantResultSalary = restorants[name].avgSalary;
-			}
+	// 	for ( let name in restorants){
+	// 		const  currentRestorant = restorants[name];
+	// 		if( currentRestorant.avgSalary > bestRestaurantResultSalary){
+	// 			bestRestaurant = {
+	// 				name,
+	// 				workers: currentRestorant[name].workers,
+	// 				bestSalary: currentRestorant[name].bestSalary,
+	// 				avgSalary: currentRestorant[name].avgSalary
+	// 			};
+	// 			bestRestaurantResultSalary = restorants[name].avgSalary;
+	// 		}
 
-		}
-		bestRestorantResult.textContent = `Name: ${bestRestaurant.name} Average Salary: ${Number(bestRestaurant.avgSalary).toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`;
+	// 	}
+	// 	bestRestorantResult.textContent = `Name: ${bestRestaurant.name} Average Salary: ${Number(bestRestaurant.avgSalary).toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`;
 
-		const result = [];
-		bestRestaurant.workers.forEach((worker) => {
-			result.push(`Name: ${worker.name} With Salary: ${worker.salary}`);
-		});
+	// 	const result = [];
+	// 	bestRestaurant.workers.forEach((worker) => {
+	// 		result.push(`Name: ${worker.name} With Salary: ${worker.salary}`);
+	// 	});
 
-		workersResult.textContent = result.join(' ');
+	// 	workersResult.textContent = result.join(' ');
 
 	}
 
@@ -153,4 +171,3 @@ function solve() {
 			workersEl.textContent += `Name: ${name} With Salary: ${salary} `;
 		});
 	}*/
-}
