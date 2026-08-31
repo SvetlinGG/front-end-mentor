8d8d4d600d2c30e3f99f0c179eac5e5f0e7baf93
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-31 09:57:24 +0200

    add new item to ul element

--- DOM/DOM-manipulation-LAB/01. List-Of-Items/app.js
+++ DOM/DOM-manipulation-LAB/01. List-Of-Items/app.js
@@ -1,5 +1,10 @@
 function addItem() {
     
-        
+    const item = document.getElementById('items');
+    const newItem = document.getElementById('newItemText').value;
+    item.innerHTML += `<li>${newItem}</li>`;
+    newItem = 'focus'
+    
+
 
 }
